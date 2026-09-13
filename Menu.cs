c0c70b3a9c6f140a2f7acdf15c28460284bde391
Daniel Medeiros <Daniel.oliveira201220@gmail.com>
2026-09-13 09:14:56 -0300

    Separação do enum Menu em arquivo único e correções de escopo no switch case do menu

---
+++ Menu.cs
@@ -0,0 +1,10 @@
+enum Menu 
+{
+CadastrarPet = 1,
+ListarPets = 2,
+EditarPet = 3,
+AdicionarVacina = 4,
+AdicionarProcedimento = 5,
+DeletarPet = 6,
+Sair = 7
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,50 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+bool executarPrograma = true;
+
+while (executarPrograma == true)
+{
+
+Console.WriteLine($"Escolha uma opção:\n1 - Cadastrar Pet\n2 - Listar pet\n3 - Editar pet\n4 - Adicionar vacina\n5 - Adicionar procedimento\n6 - Deletar pet\n7 - Sair");
+
+int opcao = Convert.ToInt32(Console.ReadLine());
+Menu opcaoMenu = (Menu)opcao;
+
+switch(opcaoMenu)
+{
+case Menu.CadastrarPet:
+{
+Console.WriteLine("Em construção");
+break;
+}
+case Menu.ListarPets:
+{
+Console.WriteLine("Em construção");
+break;
+}
+case Menu.EditarPet:
+{
+Console.WriteLine("Em construção");
+break;
+}
+case Menu.AdicionarVacina:
+{
+Console.WriteLine("Em construção");
+break;
+}
+case Menu.AdicionarProcedimento:
+{
+Console.WriteLine("Em construção");
+break;
+}
+case Menu.DeletarPet:
+{
+Console.WriteLine("Em construção");
+break;
+}
+case Menu.Sair:
+{
+executarPrograma = false;
+break;
+}
+}
+}
